912866dd40c4640879d49d2a1229a12c02d20b99
xiaoxin2202 <99644805+xiaoxin2202@users.noreply.github.com>
2024-11-21 15:47:34 +0700

    Create app.js

---
+++ Node/app.js
@@ -0,0 +1,11 @@
+export express from 'express';
+
+const app = express();
+
+app.get('/', (req, res)=> {
+  res.status(200).send('hello');
+});
+
+app.listent(3000, () => {
+  console.log('server is running');
+})
