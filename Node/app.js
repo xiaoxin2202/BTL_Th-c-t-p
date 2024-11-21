cae57e635f78652b2b206dc6b4b2c3d7d002d427
xiaoxin2202 <huytam2202@gmail.com>
2024-11-21 22:13:27 +0700

    update

--- Node/app.js
+++ Node/app.js
@@ -1,11 +1,11 @@
-export express from 'express';
+import express from 'express';
 
 const app = express();
 
 app.get('/', (req, res)=> {
-  res.status(200).send('hello');
+  res.status(200).send('Hello!');
 });
 
-app.listent(3000, () => {
-  console.log('server is nrrning');
+app.listen(3000, () => {
+  console.log('server is running');
 })
